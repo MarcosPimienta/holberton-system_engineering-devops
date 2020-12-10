10f08eba9f4c0e06be071968236abbb3529efe27
MarcosPimienta <1676@holbertonschool.com>
2020-12-10 16:41:35 +0000

    fixed file path

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #! usr/bin/env bash
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => 'etc/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare Identity':
   ensure => present,
-  path   => 'etc/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/holberton',
 }
