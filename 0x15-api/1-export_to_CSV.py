ad27e8cde45347d085ed06fa4a0c3a5805797c9d
MarcosPimienta <1676@holbertonschool.com>
2021-02-01 20:37:34 -0500

    fixing this

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,20 +5,15 @@ from sys import argv
 from requests import get
 
 if __name__ == '__main__':
-    users = requests.get('https://jsonplaceholder.typicode.com/users',
-                         params={"id": argv[1]})
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos',
-                         params={"userId": argv[1]})
+    user = get('https://jsonplaceholder.typicode.com/users/{:s}'.format(
+        argv[1]))
+    todo = get('http://jsonplaceholder.typicode.com/todos?userId={:s}'.format(
+        argv[1]))
     user_dict = user.json()
     todo_list = todo.json()
-
-    for u_name in users_dict:
-        USERNAME = u_name.get('name')
-        USER_ID = u_name.get('id')
-
-    with open("USER_ID" + ".csv", mode='w') as f:
-        csv = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
-        for tasks in todos_list:
-            TASK_STATUS = tasks.get("completed")
-            TASK_TITLE = tasks.get("title")
-            csv.writerow([USER_ID, USERNAME, TASK_STATUS, TASK_TITLE])
+    with open('{:s}.csv'.format(argv[1]), mode='w') as task_file:
+        task_writer = writer(task_file, delimiter=',', quotechar='"',
+                             quoting=QUOTE_ALL)
+        for item in todo_list:
+            task_writer.writerow([user_dict['id'], user_dict['username'],
+                                 item['completed'], item['title']])
