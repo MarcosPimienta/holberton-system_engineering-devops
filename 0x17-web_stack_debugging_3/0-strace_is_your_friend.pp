831d3a1820497ace25cd9fc5affee4468f78dad0
Baloo2017@ <1676@holbertonschool.com>
2021-02-18 16:09:29 -0500

    no slash at end

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # installs puppet-lint 2.1.1
 exec { 'modify php':
-  path    => ['/bin/', '/usr/bin/'],
+  path    => ['/bin', '/usr/bin'],
   command => 'sed -i 's/phpp/php/g' /var/www/html/wp-settings.php',
 }
